abdcd9a5419e9bc22dec15bca668c0ac2b2ea21c
AlexandreRCS <ale.maresias@gmail.com>
2023-05-12 21:26:21 -0300

    Att/05/2023
    terminado a galeria e iniciando o next/prev

--- HTML+CSS+JS/js/gallery.js
+++ HTML+CSS+JS/js/gallery.js
@@ -1,6 +1,8 @@
 var overlay = document.querySelector('.jl-overlay');
 var frameImage = document.querySelector('.jl-gallery-frame-image');
+var frameContainer = document.querySelector('.jl-gallery-frame-container');
 var galleryImagens = document.querySelectorAll('.jl-thumb-img');
+var closeGallery = document.querySelectorAll('.jl-toggle-gallery');
 
 const getImagesSrc = function (){
     for (var i = 0; i < galleryImagens.length; i ++){
@@ -8,12 +10,31 @@ const getImagesSrc = function (){
             var imageSrc = this.getAttribute('data-src');
             frameImage.setAttribute('src', imageSrc);
 
-            overlay.classList.toggle('jl-is-open');
+            overlay.classList.add('jl-is-open');
+            frameContainer.classList.add('jl-is-open');
         })
         
 
     }
 }
 
-
 getImagesSrc();
+
+for (var c = 0; c < closeGallery.length; c ++){
+    closeGallery[c].addEventListener('click',function(){
+        overlay.classList.remove('jl-is-open');
+        frameContainer.classList.remove('jl-is-open');
+    })
+}
+
+//IDENTIFICAMOS O ITEM ATUAL NO FRAME
+
+
+//DEFINIMOS O NUMERO DO PROXIMO ITEM = ATUAL +  1
+
+// FAZEMOS  O LOOP E IDENTIFICAMOS QUAL ITEM FAZ MATCH COM O NUMERO PREOXIMO ITEM 
+
+// CAPTURAMOS O DATA-SRC
+
+
+// PASSAMOS O DATA-SR PARA A TAG DE IMG NO FRAME

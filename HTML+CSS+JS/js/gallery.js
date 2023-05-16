abf72438ba8bfc158d5fc03584b8d8d54a2f99ed
AlexandreRCS <ale.maresias@gmail.com>
2023-05-15 22:42:26 -0300

    Att/15/05/23
    adicionado o next e prev do frame

--- HTML+CSS+JS/js/gallery.js
+++ HTML+CSS+JS/js/gallery.js
@@ -3,12 +3,17 @@ var frameImage = document.querySelector('.jl-gallery-frame-image');
 var frameContainer = document.querySelector('.jl-gallery-frame-container');
 var galleryImagens = document.querySelectorAll('.jl-thumb-img');
 var closeGallery = document.querySelectorAll('.jl-toggle-gallery');
+var btnNext = document.querySelector('.jl-item-next');
+var btnPrev = document.querySelector('.jl-item-prev');
 
 const getImagesSrc = function (){
     for (var i = 0; i < galleryImagens.length; i ++){
         galleryImagens[i].addEventListener('click', function (){
             var imageSrc = this.getAttribute('data-src');
+            var itemNum = this.getAttribute('data-item');
+
             frameImage.setAttribute('src', imageSrc);
+            frameImage.setAttribute('data-index', itemNum);
 
             overlay.classList.add('jl-is-open');
             frameContainer.classList.add('jl-is-open');
@@ -27,14 +32,59 @@ for (var c = 0; c < closeGallery.length; c ++){
     })
 }
 
-//IDENTIFICAMOS O ITEM ATUAL NO FRAME
+const nexItem = function(){
 
+    //IDENTIFICAMOS O ITEM ATUAL NO FRAME
+    var currItemNum = frameImage.getAttribute('data-index');
 
-//DEFINIMOS O NUMERO DO PROXIMO ITEM = ATUAL +  1
+    //DEFINIMOS O NUMERO DO PROXIMO ITEM = ATUAL +  1
+    var nextItemNum = parseInt(currItemNum) + 1;
 
-// FAZEMOS  O LOOP E IDENTIFICAMOS QUAL ITEM FAZ MATCH COM O NUMERO PREOXIMO ITEM 
+    // FAZEMOS  O LOOP E IDENTIFICAMOS QUAL ITEM FAZ MATCH COM O NUMERO PREOXIMO ITEM 
+    for(var n = 0 ; n < galleryImagens.length; n ++){
+        var item = galleryImagens[n];
+        var itemNum = parseInt(item.getAttribute('data-item'));
 
-// CAPTURAMOS O DATA-SRC
+        if (itemNum === nextItemNum){
+            // CAPTURAMOS O DATA-SRC
+            var nextSrc = item.getAttribute('data-src');
+            var nextIndex = item.getAttribute('data-item');
 
+            // PASSAMOS O DATA-SR PARA A TAG DE IMG NO FRAME
+            frameImage.setAttribute('src', nextSrc);
+            frameImage.setAttribute('data-index', nextIndex);
+        }
+    }
+}
 
-// PASSAMOS O DATA-SR PARA A TAG DE IMG NO FRAME
+btnNext.addEventListener('click', function(){
+    nexItem();
+});
+
+const prevItem = function(){
+    // IDENTIFICANDO O FRAME 
+    var currItemNum = frameImage.getAttribute('data-index');
+
+    // DEFIIMOS O  NUMERO DO PREV ITEM
+    var prevItemNum = parseInt(currItemNum) - 1;
+
+    //FAZEMOS O LOOP E IDENTIFICAMOS QUAL ITEM FAZ MACTH COM O PREV ITEM 
+    for(var p = 0; p < galleryImagens.length; p ++){
+        var item = galleryImagens[p];
+        var itemNum = parseInt(item.getAttribute('data-item'));
+
+        if (itemNum === prevItemNum){
+            var prevSrc = item.getAttribute('data-src');
+            var prevIndex = item.getAttribute('data-item');
+    
+            // PASSAMOS O DATA-SRC E DATA-INDEX A TAG IMG
+            frameImage.setAttribute('src', prevSrc);
+            frameImage.setAttribute('data-index', prevIndex);
+        }
+
+    }
+}
+
+btnPrev.addEventListener('click', function(){
+    prevItem();
+})
